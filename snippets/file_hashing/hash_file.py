bb7710ab0daa52d63c09a548d0e0e14363302e0b
SporadicSpore <50343993+SporadicSpore@users.noreply.github.com>
2021-02-25 09:33:07 -0600

    Remove redundant if, formatting

--- snippets/file_hashing/hash_file.py
+++ snippets/file_hashing/hash_file.py
@@ -3,14 +3,13 @@ import click
 
 
 @click.command()
-@click.argument("filename",type=click.File("rb"))
+@click.argument("filename", type=click.File("rb"))
 @click.option("--hash", default="md5", show_default=True,
 type=click.Choice(hashlib.algorithms_guaranteed, case_sensitive=False),
 help="Hashing algorithm to use")
 def get_hash(filename, hash):
     """Returns the hash of FILENAME."""
-    if hash.lower() in hashlib.algorithms_guaranteed:
-        file_hash = getattr(hashlib,hash.lower())()
+    file_hash = getattr(hashlib, hash.lower())()
 
     while True:
         while chunk := filename.read(8192):
